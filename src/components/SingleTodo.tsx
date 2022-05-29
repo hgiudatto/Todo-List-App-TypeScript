7b68eea40cfe2b08721ae7e75bf5f76716dec4a1
hgiudatto <hgiudatto@prismamp.com>
2022-05-29 15:00:25 -0300

    handleDone handleDelete

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,15 +1,50 @@
 import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import TodoList from "./TodoList";
 
 type Props = {
   todo: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
+  isDone?: false;
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo } : todo)));
+  };
+
+  const handleDelete = (id: number) => {
+      setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
   return (
     <form className="todos__single">
-      <span className="todos__single--text">{todo.todo}</span>
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
+
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon" onClick={() => {
+            handleDelete(todo.id);
+          }}>
+          <AiFillDelete />
+        </span>
+        <span
+          className="icon"
+          onClick={() => {
+            handleDone(todo.id);
+          }}
+        >
+          <MdDone />
+        </span>
+      </div>
     </form>
   );
 };
